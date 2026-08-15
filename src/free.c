745644f071e944755eb0e7d281ec6175d31e9245
giopiro99 <giovannipirozzi12345@gmail.com>
2026-08-15 23:15:35 +0200

    iniziato free

--- src/free.c
+++ src/free.c
@@ -1,8 +1,40 @@
-#include "../includes/malloc.h"
+#include <stdlib.h>
+
+void    dealloc_tiny_ptr(void *ptr){
+    (void)ptr;
+}
+
+void    dealloc_small_ptr(void *ptr){
+    (void)ptr;
+}
+
+void    dealloc_large_ptr(void *ptr){
+    (void)ptr;
+}
+
+void*    try_free_tiny_ptr(void* ptr){
+    return (NULL);
+}
+
+void*    try_free_small_ptr(void* ptr){
+    return (NULL);
+}
+
+void*    try_free_large_ptr(void* ptr){
+    return (NULL);
+}
 
 void    free(void *ptr){
     if (ptr == NULL) return;
 
-    
+    if (try_free_tiny_ptr(ptr)){
+        return ;
+    }
+    if (try_free_small_ptr(ptr)){
+        return ;
+    }
+    if (try_free_large_ptr(ptr)){
+        return ;
+    }
     return ;
 }
